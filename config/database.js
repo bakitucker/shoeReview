28d355c50a2dab201daa21cfac1a4b0efbdf8796
bakitucker <bakitucker8@gmail.com>
2023-04-30 21:18:20 -0400

    functional need to fix the trash can and add more css

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://bakitucker8:baki123@fullstackpersonaltwo.cd88bxj.mongodb.net/fullstackPersonalTwo?retryWrites=true&w=majority', 
+    'dbName': 'fullstackPersonalTwo'
+};
